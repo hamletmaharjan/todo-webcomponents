99b5c0fb056ac7973cdfcc4302ca974c90226702
hamlet <imhamlet.hams@gmail.com>
2021-05-03 09:57:42 +0545

    remove unused codes

--- script.js
+++ script.js
@@ -78,36 +78,7 @@ class Todo extends HTMLElement {
                 this.deleteTodo(e.detail.id)
             });
             myUl.appendChild(todoItem);
-            // console.log(todoItem);
-            // child.appendChild(todoItem)
-            // child.innerHTML = item.title;
-
-            // let article = document.createElement('app-article');
-            // article.setAttribute('title', item.title);
-            // myUl.appendChild(article);
-
-            // let checkbox = document.createElement('input');
-            // checkbox.setAttribute('type', 'checkbox');
-            // checkbox.addEventListener('change', (e) => {
-            //     this.toggleTodo(item.id);
-            // });
-            // if(item.completed){
-            //     checkbox.setAttribute('checked', item.completed);
-            // }
-            // let label = document.createElement('label');
-            // if(item.completed){
-            //     label.style.textDecoration = 'line-through';
-            // }
-            // label.innerHTML = item.title;
-            // let button = document.createElement('button');
-            // button.setAttribute('id', 'delete-btn');
-            // button.innerHTML = '&times;';
-            // button.addEventListener('click', (e)=>this.deleteTodo(item.id));
             
-            // child.appendChild(checkbox);
-            // child.appendChild(label);
-            // child.appendChild(button);
-            // myUl.appendChild(child);
         });
 
     }
